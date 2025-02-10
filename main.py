eab8c9b46398a01fa3bd61de9746879f69afb552
Nightmer71 <totharon71@gmail.com>
2025-02-10 10:17:05 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 class ParkingLot:
     def __init__(self, total_spots = 10):
 
-        self.parking_spots
+        self.parking_spots = {i: 'Avaliable' for i in range(1, total_spots + 1)}
+
+    def find_avaliable(self):
+
+        avaliable = [spot for spot, status in self.parking_spots.items() if status == 'Avaliable']
+
+        if avaliable:
+
+            print(f"Avaliable spots: {avaliable}")
+
+        else:
+
+            print("No parking spots avaliable")
